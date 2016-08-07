9cb903853450c80662eb91bb3e437c3d0c9c1c6b
Joe Fearnley <joe.fearnley@gmail.com>
2016-08-06 23:55:54 -0400

    Adding init vue stuff

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 (function() {
     var App = new Framework7();
+
+    new Vue({
+        el: '#stories',
+        data: {
+            stories: []
+        },
+        ready: function() {
+            this.fetchStories();
+        },
+        methods: function() {
+            fetchStories: function() {
+
+            }
+        }
+    });
 })();
