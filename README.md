ee9b4eceb84b7f7d7e14535a43231d5d576b7bed
Joe Fearnley <joe.fearnley@gmail.com>
2016-08-17 18:21:38 -0400

    Adding link in read me and changing title

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mhnr
 Mobile Hacker News Reader
+
+Check it out - http://joefearnley.com/mhnr/
--- js/app.js
+++ js/app.js
@@ -13,8 +13,6 @@
             fetchStories: function() {
                 this.$http.get('http://hn.algolia.com/api/v1/search?tags=front_page')
                     .then(function successCallback(response) {
-
-                        console.log(response.data.hits);
                         this.stories = response.data.hits;
                     }, function errorCallback(error) {
                         console.error(error);
