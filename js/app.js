39fd3eb401cfe8c8145318ab730b71226a976760
Joe Fearnley <joe.fearnley@gmail.com>
2016-08-17 18:31:34 -0400

    Adding preloader

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@
     new Vue({
         el: '#stories',
         data: {
-            stories: []
+            stories: [],
+            loading: true,
+            error: false
         },
         ready: function() {
             this.fetchStories();
@@ -13,6 +15,7 @@
             fetchStories: function() {
                 this.$http.get('http://hn.algolia.com/api/v1/search?tags=front_page')
                     .then(function successCallback(response) {
+                        this.loading = false;
                         this.stories = response.data.hits;
                     }, function errorCallback(error) {
                         console.error(error);
